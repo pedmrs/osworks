229299fed7aed643ea4e68d68769c7329a9fd73d
Pedro <pemoraes1@gmail.com>
2020-08-24 17:24:27 -0300

    Adiciona anotações para a entidade Cliente e implementa Interface para ClienteRepository

--- src/main/java/com/pedro/osworks/api/controller/ClienteController.java
+++ src/main/java/com/pedro/osworks/api/controller/ClienteController.java
@@ -1,30 +1,29 @@
 package com.pedro.osworks.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pedro.osworks.domain.model.Cliente;
+import com.pedro.osworks.domain.repository.ClienteRepository;
 
 @RestController
 public class ClienteController {
 
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("bruna");
-		cliente1.setEmail("bruna@gmail.com");
-		cliente1.setTelefone("21 999999999");
-		
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("pedro");
-		cliente2.setEmail("pedro@gmail.com");
-		cliente2.setTelefone("telegram");
-		
-		return Arrays.asList(cliente1, cliente2);
+		return clienteRepository.findAll();
+	}
+	
+	@GetMapping("/clientes/{clienteId}")
+	public Cliente buscar(@PathVariable Long clienteId) {
+		return clienteRepository.findById(clienteId).orElse(null);
 	}
 }
