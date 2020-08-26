64fddef8c35f769438507290abefc89d3c3c3bf2
Pedro <pemoraes1@gmail.com>
2020-08-26 18:01:38 -0300

    Adiciona dependencia ModelMapper para gerenciar os DTO's mais facilmente

--- src/main/java/com/pedro/osworks/api/controller/OrdemServicoController.java
+++ src/main/java/com/pedro/osworks/api/controller/OrdemServicoController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import javax.validation.Valid;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pedro.osworks.api.model.OrdemServicoRepresentationModel;
 import com.pedro.osworks.domain.model.OrdemServico;
 import com.pedro.osworks.domain.repository.OrdemServicoRepository;
 import com.pedro.osworks.domain.service.GestaoOrdemServicoService;
@@ -30,6 +32,9 @@ public class OrdemServicoController {
 	@Autowired
 	private OrdemServicoRepository ordemServicoRepository;
 	
+	@Autowired
+	private ModelMapper modelMapper;
+	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public OrdemServico criar(@Valid @RequestBody OrdemServico ordemServico) {
@@ -42,11 +47,13 @@ public class OrdemServicoController {
 	}
 	
 	@GetMapping("/{ordemServicoId}")
-	public ResponseEntity<OrdemServico> buscar(@PathVariable Long ordemServicoId) {
+	public ResponseEntity<OrdemServicoRepresentationModel> buscar(@PathVariable Long ordemServicoId) {
 		Optional<OrdemServico> ordemServico = ordemServicoRepository.findById(ordemServicoId);
 		
-		if(ordemServico.isPresent())
-			return ResponseEntity.ok(ordemServico.get());
+		if(ordemServico.isPresent()) {
+			OrdemServicoRepresentationModel model = modelMapper.map(ordemServico, OrdemServicoRepresentationModel.class);
+			return ResponseEntity.ok(model);
+		}
 		
 		return ResponseEntity.notFound().build();
 	}
