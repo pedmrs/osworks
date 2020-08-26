a38651829fc27c8fed8d09436b69f2f349e4eec7
Pedro <pemoraes1@gmail.com>
2020-08-26 18:20:53 -0300

    Cria e utiliza em OrdemServico DTO para a entidade Cliente

--- src/main/java/com/pedro/osworks/api/model/OrdemServicoRepresentationModel.java
+++ src/main/java/com/pedro/osworks/api/model/OrdemServicoRepresentationModel.java
@@ -8,7 +8,7 @@ import com.pedro.osworks.domain.model.StatusOrdemServico;
 public class OrdemServicoRepresentationModel {
 
 	private Long id;
-	private String nomeCliente;
+	private ClienteRepresentationModel cliente;
 	private String descricao;
 	private BigDecimal preco;
 	private StatusOrdemServico status;
@@ -21,11 +21,11 @@ public class OrdemServicoRepresentationModel {
 	public void setId(Long id) {
 		this.id = id;
 	}
-	public String getNomeCliente() {
-		return nomeCliente;
+	public ClienteRepresentationModel getCliente() {
+		return cliente;
 	}
-	public void setNomeCliente(String nomeCliente) {
-		this.nomeCliente = nomeCliente;
+	public void setCliente(ClienteRepresentationModel cliente) {
+		this.cliente = cliente;
 	}
 	public String getDescricao() {
 		return descricao;
